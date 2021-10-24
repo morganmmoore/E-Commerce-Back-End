97c51e595da3f4d7635d06282013bba645e1299f
morganmmoore <morganmmoore0@gmail.com>
2021-10-24 12:56:00 -0400

    Updated models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Products.belongsTo(Category, {
+  foreignKey: '',
+});
 
 // Categories have many Products
+Category.hasMany(Products, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: '',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: '',
+});
 
 module.exports = {
   Product,
